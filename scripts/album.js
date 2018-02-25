0cf97841e2390529e2375b92e6e9f119ceaa4a46
tranteresa <tranteresaat@gmail.com>
2018-02-25 14:02:19 -0800

    Assignment 6: DOMScripting Album View

---
+++ scripts/album.js
@@ -0,0 +1,97 @@
+ // Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+     ]
+};
+
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+//Assignment 6 - DOM Scripting Album View
+var albumKhalid = {
+    title: 'American Teen',
+    artist: 'Khalid',
+    label: 'Right Hand',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/22.jpeg',
+    songs: [
+        { title: 'American Teen', duration: '4:11' },
+        { title: 'Let\'s Go', duration: '3:25' },
+        { title: 'Location', duration: '3:39'},
+        { title: 'Therpy', duration: '4:19' },
+        { title: 'Saved', duration: '3:26'}
+     ]
+ };
+
+var createSongRow = function(songNumber, songName, songLength) {
+    var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+    return template;
+ };
+
+// Select elements that we want to populate with text dynamically
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
+    // Assign values to each part of the album (text,images)
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumImage.setAttribute('src', album.albumArtUrl);
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+ 
+    // Clear contents of album song list container
+    albumSongList.innerHTML = '';
+ 
+    // Build list of songs from album JavaScript object
+    for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+    }
+};
+ 
+window.onload = function() {
+    setCurrentAlbum(albumPicasso);
+
+    var albums = [albumPicasso, albumMarconi, albumKhalid];
+    var index = 0;
+
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
+};
